5356a8b5f35e0fa5f7865f84cda767d83a7b526b
Páez Ferreira Carlos David <cpaezfer@banrep.gov.co>
2023-12-13 15:03:48 -0500

    Add app user mocks

---
+++ src/app/shared/mocks/index.ts
@@ -0,0 +1,14 @@
+import { AppUserType } from "../types";
+
+
+export const APP_USER: AppUserType = {
+    id: 'cpaezfer',
+    name: "Carlos David Páez Ferreira",
+    appRole: 'admin',
+    email: 'cpaezfer@banrep.gov.co',
+    password: 'pass123',
+    username: 'cpaezfer',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null,
+};
