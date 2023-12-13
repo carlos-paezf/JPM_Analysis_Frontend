a098cb6aaa7584506a2f866af29a9936fa99ca69
Páez Ferreira Carlos David <cpaezfer@banrep.gov.co>
2023-12-13 13:46:10 -0500

    Init basic app modules

---
+++ src/app/protected/modules/user-entitlements/user-entitlements.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserEntitlementsRoutingModule } from './user-entitlements-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    UserEntitlementsRoutingModule
+  ]
+})
+export class UserEntitlementsModule { }
