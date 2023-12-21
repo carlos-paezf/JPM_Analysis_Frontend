e26696029a085eca6afdb1e4a3b5c78efe6e6d94
Páez Ferreira Carlos David <cpaezfer@banrep.gov.co>
2023-12-20 22:35:44 -0500

    Update Imports in Protected Module

--- src/app/protected/protected.module.ts
+++ src/app/protected/protected.module.ts
@@ -1,14 +1,21 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { MaterialDesignModule } from '../material-design/material-design.module';
+import { SharedModule } from '../shared/shared.module';
+import { MainComponent } from './pages/main/main.component';
 import { ProtectedRoutingModule } from './protected-routing.module';
 
 
-@NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    ProtectedRoutingModule
-  ]
-})
+@NgModule( {
+    declarations: [
+        MainComponent
+    ],
+    imports: [
+        CommonModule,
+        ProtectedRoutingModule,
+        SharedModule,
+        MaterialDesignModule
+    ]
+} )
 export class ProtectedModule { }
