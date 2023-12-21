5aa65bcc72e4ae491ed3cfa9b9eb8e862247e9a5
Páez Ferreira Carlos David <cpaezfer@banrep.gov.co>
2023-12-20 23:18:52 -0500

    Update App Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 import { isLoggedGuard } from './shared/guards/is-logged.guard';
+import { noActiveSessionGuard } from './shared/guards/no-active-session.guard';
 
 
 const routes: Routes = [
-    /* {
+    {
         path: 'auth',
         canActivateChild: [ noActiveSessionGuard ],
         canMatch: [ noActiveSessionGuard ],
         loadChildren: () => import( './auth/auth.module' ).then( m => m.AuthModule )
-    }, */
+    },
     {
         path: 'app',
         canActivateChild: [ isLoggedGuard ],
--- src/app/protected/protected-routing.module.ts
+++ src/app/protected/protected-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { NotFoundComponent } from '../shared/components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: MainComponent
+    },
+    {
+        path: '**', component: NotFoundComponent, data: { title: '404 - No encontrado' }
+    }
+];
 
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
+@NgModule( {
+    imports: [ RouterModule.forChild( routes ) ],
+    exports: [ RouterModule ]
+} )
 export class ProtectedRoutingModule { }
