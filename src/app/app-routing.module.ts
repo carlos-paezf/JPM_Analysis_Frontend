50f05fe3c8d279a8b1890fae9850aa838262cb54
Páez Ferreira Carlos David <cpaezfer@banrep.gov.co>
2023-12-20 22:40:25 -0500

    Update app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { isLoggedGuard } from './shared/guards/is-logged.guard';
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
+
+const routes: Routes = [
+    /* {
+        path: 'auth',
+        canActivateChild: [ noActiveSessionGuard ],
+        canMatch: [ noActiveSessionGuard ],
+        loadChildren: () => import( './auth/auth.module' ).then( m => m.AuthModule )
+    }, */
+    {
+        path: 'app',
+        canActivateChild: [ isLoggedGuard ],
+        canMatch: [ isLoggedGuard ],
+        loadChildren: () => import( './protected/protected.module' ).then( m => m.ProtectedModule )
+    },
+    {
+        path: '', pathMatch: 'full', redirectTo: 'app'
+    },
+    {
+        path: '**', component: NotFoundComponent, data: { title: '404 - No encontrado' }
+    }
+];
+
+@NgModule( {
+    imports: [ RouterModule.forRoot( routes ) ],
+    exports: [ RouterModule ]
+} )
 export class AppRoutingModule { }
