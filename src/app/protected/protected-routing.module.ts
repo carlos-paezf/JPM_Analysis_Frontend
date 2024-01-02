2bddf61c63fc07508cf0fb92e903d5806fae3854
Páez Ferreira Carlos David <cpaezfer@banrep.gov.co>
2024-01-02 12:09:14 -0500

    Update Protecting Routes

--- src/app/protected/protected-routing.module.ts
+++ src/app/protected/protected-routing.module.ts
@@ -6,7 +6,45 @@ import { NotFoundComponent } from '../shared/components/not-found/not-found.comp
 const routes: Routes = [
     {
         path: '',
-        component: MainComponent
+        component: MainComponent,
+        children: [
+            {
+                path: 'file-upload',
+                loadChildren: () => import( './modules/files/files.module' ).then( m => m.FilesModule )
+            },
+            {
+                path: 'accounts',
+                loadChildren: () => import( './modules/accounts/accounts.module' ).then( m => m.AccountsModule )
+            },
+            {
+                path: 'actions',
+                loadChildren: () => import( './modules/actions/actions.module' ).then( m => m.ActionsModule )
+            },
+            {
+                path: 'clients',
+                loadChildren: () => import( './modules/clients/clients.module' ).then( m => m.ClientsModule )
+            },
+            {
+                path: 'company-users',
+                loadChildren: () => import( './modules/company-users/company-users.module' ).then( m => m.CompanyUsersModule )
+            },
+            {
+                path: 'functions',
+                loadChildren: () => import( './modules/functions/functions.module' ).then( m => m.FunctionsModule )
+            },
+            {
+                path: 'products',
+                loadChildren: () => import( './modules/products/products.module' ).then( m => m.ProductsModule )
+            },
+            {
+                path: 'profiles',
+                loadChildren: () => import( './modules/profiles/profiles.module' ).then( m => m.ProfilesModule )
+            },
+            {
+                path: 'user-entitlements',
+                loadChildren: () => import( './modules/user-entitlements/user-entitlements.module' ).then( m => m.UserEntitlementsModule )
+            },
+        ]
     },
     {
         path: '**', component: NotFoundComponent, data: { title: '404 - No encontrado' }
