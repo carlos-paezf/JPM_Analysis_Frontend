36e9efeb4ed32f5a4429e3e979b8de728b12198d
Páez Ferreira Carlos David <cpaezfer@banrep.gov.co>
2024-01-03 09:40:44 -0500

    Add Dashboard Constants

---
+++ src/app/protected/modules/dashboard/constants/modules.ts
@@ -0,0 +1,83 @@
+import { ModulesPanelsType } from "../../../../shared/types";
+
+
+export const APP_MODULES_PANELS: ModulesPanelsType[] = [
+    {
+        title: 'Archivos',
+        panels: [
+            {
+                label: 'Carga de archivos',
+                routeLink: 'file-upload'
+            }
+        ]
+    },
+    {
+        title: 'Perfiles',
+        panels: [
+            {
+                label: 'Listado de perfiles',
+                routeLink: 'profiles'
+            },
+            {
+                label: 'Usuarios por perfil',
+                routeLink: 'profiles/users-by-profiles'
+            },
+            {
+                label: 'Acciones',
+                routeLink: 'profiles/actions'
+            },
+            {
+                label: 'Funciones',
+                routeLink: 'profiles/functions'
+            }
+        ]
+    },
+    {
+        title: 'Company Users',
+        panels: [
+            {
+                label: 'Usuarios de la compañía',
+                routeLink: 'company-users'
+            }
+        ]
+    },
+    {
+        title: 'Users Entitlements',
+        panels: [
+            {
+                label: 'Títulos de los usuarios',
+                routeLink: 'user-entitlements'
+            }
+        ]
+    },
+    {
+        title: 'JPM Products',
+        panels: [
+            {
+                label: 'Productos JPM',
+                routeLink: 'products'
+            },
+            {
+                label: 'Cuentas',
+                routeLink: 'accounts'
+            },
+            {
+                label: 'Funciones',
+                routeLink: 'functions'
+            }
+        ]
+    },
+    {
+        title: 'APP Users',
+        panels: [
+            {
+                label: 'Listado de usuarios de la aplicación',
+                routeLink: 'app-users/list-app-user'
+            },
+            {
+                label: 'Crear nuevo usuario de la aplicación',
+                routeLink: 'app-users/create-app-user'
+            }
+        ]
+    }
+];
