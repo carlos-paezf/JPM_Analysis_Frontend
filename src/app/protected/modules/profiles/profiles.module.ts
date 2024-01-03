53f09f0c8b59706f497b8e412893ef99dd523818
Páez Ferreira Carlos David <cpaezfer@banrep.gov.co>
2024-01-03 18:57:39 -0500

    Update Profile Module Class

--- src/app/protected/modules/profiles/profiles.module.ts
+++ src/app/protected/modules/profiles/profiles.module.ts
@@ -1,14 +1,25 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { MaterialDesignModule } from '../../../material-design/material-design.module';
+import { SharedModule } from '../../../shared/shared.module';
+import { AdminProfilesComponent } from './pages/admin-profiles/admin-profiles.component';
+import { ListProfilesComponent } from './pages/list-profiles/list-profiles.component';
 import { ProfilesRoutingModule } from './profiles-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
-@NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    ProfilesRoutingModule
-  ]
-})
+@NgModule( {
+    declarations: [
+        ListProfilesComponent,
+        AdminProfilesComponent
+    ],
+    imports: [
+        CommonModule,
+        SharedModule,
+        ReactiveFormsModule,
+        MaterialDesignModule,
+        ProfilesRoutingModule
+    ]
+} )
 export class ProfilesModule { }
