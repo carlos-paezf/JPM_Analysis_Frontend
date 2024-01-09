b79b6185cf2355f4e716e2252c54433c8282ea25
Páez Ferreira Carlos David <cpaezfer@banrep.gov.co>
2024-01-09 16:15:21 -0500

    Update Profiles Services

--- src/app/protected/modules/profiles/services/profiles.service.ts
+++ src/app/protected/modules/profiles/services/profiles.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable, delay, of } from 'rxjs';
 
 import { COMPANY_USERS, PROFILES } from '../../../../shared/mocks';
-import { CompanyUserType, ProfileType, ResponseSheetsType } from '../../../../shared/types';
+import { CompanyUserType, FunctionType, ProfileType, ResponseSheetsType } from '../../../../shared/types';
+import { FunctionsService } from './functions.service';
 
 
 @Injectable( {
@@ -10,7 +11,7 @@ import { CompanyUserType, ProfileType, ResponseSheetsType } from '../../../../sh
 } )
 export class ProfilesService {
 
-    constructor () { }
+    constructor ( private _functionsService: FunctionsService ) { }
 
     public getProfiles (): Observable<ResponseSheetsType<ProfileType>> {
         const response: ResponseSheetsType<ProfileType> = {
@@ -38,11 +39,24 @@ export class ProfilesService {
         return of( response ).pipe( delay( Math.random() * 1000 ) );
     }
 
-    public updateProfile ( id: string, updatedData: ProfileType ): Observable<boolean> {
+    public updateProfile ( id: string, updatedData: ProfileType, associatedFunctions: FunctionType[] ): Observable<boolean> {
+        const index = PROFILES.findIndex( profile => profile.id === id );
+
+        this._functionsService.updateProfileFunctions( id, associatedFunctions );
+
+        if ( index !== -1 ) {
+            // PROFILES[ index ] = { ...updatedData, id: PROFILES[ index ].id };
+            PROFILES[ index ] = { ...PROFILES[ index ], ...updatedData, updated_at: new Date() };
+        }
+
+        return of( true );
+    }
+
+    public deleteProfile ( id: string ): Observable<boolean> {
         const index = PROFILES.findIndex( profile => profile.id === id );
 
         if ( index !== -1 ) {
-            PROFILES[ index ] = { ...updatedData, id: PROFILES[ index ].id };
+            PROFILES[ index ] = { ...PROFILES[ index ], deleted_at: new Date() };
         }
 
         return of( true );
