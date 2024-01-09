f4ef1c5f5893dbe042eb697957df28b51877305f
Páez Ferreira Carlos David <cpaezfer@banrep.gov.co>
2024-01-09 16:13:42 -0500

    Update Profiles routing

--- src/app/protected/modules/profiles/profiles-routing.module.ts
+++ src/app/protected/modules/profiles/profiles-routing.module.ts
@@ -1,10 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NotFoundComponent } from '../../../shared/components/not-found/not-found.component';
+import { AdminProfilesComponent } from './pages/admin-profiles/admin-profiles.component';
+import { ListProfilesComponent } from './pages/list-profiles/list-profiles.component';
+import { UsersByProfilesComponent } from './pages/users-by-profiles/users-by-profiles.component';
 
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
+
+const routes: Routes = [
+    {
+        path: 'list-profiles',
+        component: ListProfilesComponent,
+        data: { title: 'JPM Lista de Perfiles' },
+        pathMatch: 'full'
+    },
+    {
+        path: 'admin/:id',
+        component: AdminProfilesComponent,
+        pathMatch: 'full',
+        data: { title: 'JPM Detalle de Usuario' }
+    },
+    {
+        path: 'users-by-profiles',
+        component: UsersByProfilesComponent,
+        pathMatch: 'full',
+        data: { title: 'Usuarios por Perfiles' }
+    },
+    {
+        path: '', redirectTo: 'list-profiles', pathMatch: 'full'
+    },
+    {
+        path: '**', component: NotFoundComponent,
+        data: { title: '404 - No encontrado' }
+    }
+];
+
+
+@NgModule( {
+    imports: [ RouterModule.forChild( routes ) ],
+    exports: [ RouterModule ]
+} )
 export class ProfilesRoutingModule { }
