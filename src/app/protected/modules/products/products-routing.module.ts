ba0bf80e2ceba49869223caf14c7b031361495b0
Páez Ferreira Carlos David <cpaezfer@banrep.gov.co>
2024-01-24 13:30:22 -0500

    Add Products Routing

--- src/app/protected/modules/products/products-routing.module.ts
+++ src/app/protected/modules/products/products-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NotFoundComponent } from '../../../shared/components/not-found/not-found.component';
+import { AdminProductsComponent } from './pages/admin-products/admin-products.component';
+import { ListProductsComponent } from './pages/list-products/list-products.component';
 
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
+
+const routes: Routes = [
+    {
+        path: 'list-products',
+        component: ListProductsComponent,
+        data: { title: 'JPM Lista de Productos' },
+        pathMatch: 'full'
+    },
+    {
+        path: 'admin/:id',
+        component: AdminProductsComponent,
+        pathMatch: 'full',
+        data: { title: 'JPM Detalle de Producto' }
+    },
+    {
+        path: '', redirectTo: 'list-products', pathMatch: 'full'
+    },
+    {
+        path: '**', component: NotFoundComponent,
+        data: { title: '404 - No encontrado' }
+    }
+];
+
+
+@NgModule( {
+    imports: [ RouterModule.forChild( routes ) ],
+    exports: [ RouterModule ]
+} )
 export class ProductsRoutingModule { }
