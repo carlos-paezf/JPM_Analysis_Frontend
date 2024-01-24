92dd35dafd2f0e2c486d961ce30bb1883fcad235
Páez Ferreira Carlos David <cpaezfer@banrep.gov.co>
2024-01-24 13:31:24 -0500

    Update Clients Module

--- src/app/protected/modules/clients/clients.module.ts
+++ src/app/protected/modules/clients/clients.module.ts
@@ -1,14 +1,23 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { MaterialDesignModule } from '../../../material-design/material-design.module';
+import { SharedModule } from '../../../shared/shared.module';
 import { ClientsRoutingModule } from './clients-routing.module';
+import { ListClientsComponent } from './pages/list-clients/list-clients.component';
 
 
-@NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    ClientsRoutingModule
-  ]
-})
+@NgModule( {
+    declarations: [
+        ListClientsComponent
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        SharedModule,
+        MaterialDesignModule,
+        ClientsRoutingModule
+    ]
+} )
 export class ClientsModule { }
