1e29dd3763c35eecf2f3f3e8fa19a750f3f40cb0
Páez Ferreira Carlos David <cpaezfer@banrep.gov.co>
2024-01-24 13:29:56 -0500

    Add Accounts Routing

--- src/app/protected/modules/accounts/accounts-routing.module.ts
+++ src/app/protected/modules/accounts/accounts-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NotFoundComponent } from '../../../shared/components/not-found/not-found.component';
+import { AdminAccountsComponent } from './pages/admin-accounts/admin-accounts.component';
+import { ListAccountsComponent } from './pages/list-accounts/list-accounts.component';
 
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
+
+const routes: Routes = [
+    {
+        path: 'list-accounts',
+        component: ListAccountsComponent,
+        data: { title: 'JPM Lista de Cuentas' },
+        pathMatch: 'full'
+    },
+    {
+        path: 'admin/:id',
+        component: AdminAccountsComponent,
+        pathMatch: 'full',
+        data: { title: 'JPM Detalle de Cuenta' }
+    },
+    {
+        path: '', redirectTo: 'list-accounts', pathMatch: 'full'
+    },
+    {
+        path: '**', component: NotFoundComponent,
+        data: { title: '404 - No encontrado' }
+    }
+];
+
+
+@NgModule( {
+    imports: [ RouterModule.forChild( routes ) ],
+    exports: [ RouterModule ]
+} )
 export class AccountsRoutingModule { }
