ef08c1f2afa8215737b9551c40db5d9a1419e650
Páez Ferreira Carlos David <cpaezfer@banrep.gov.co>
2024-01-24 13:31:48 -0500

    Update User Entitlements Module

--- src/app/protected/modules/user-entitlements/user-entitlements.module.ts
+++ src/app/protected/modules/user-entitlements/user-entitlements.module.ts
@@ -1,14 +1,25 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { MaterialDesignModule } from '../../../material-design/material-design.module';
+import { SharedModule } from '../../../shared/shared.module';
+import { AdminUserEntitlementsComponent } from './pages/admin-user-entitlements/admin-user-entitlements.component';
+import { ListUserEntitlementsComponent } from './pages/list-user-entitlements/list-user-entitlements.component';
 import { UserEntitlementsRoutingModule } from './user-entitlements-routing.module';
 
 
-@NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    UserEntitlementsRoutingModule
-  ]
-})
+@NgModule( {
+    declarations: [
+        AdminUserEntitlementsComponent,
+        ListUserEntitlementsComponent
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        SharedModule,
+        MaterialDesignModule,
+        UserEntitlementsRoutingModule
+    ]
+} )
 export class UserEntitlementsModule { }
