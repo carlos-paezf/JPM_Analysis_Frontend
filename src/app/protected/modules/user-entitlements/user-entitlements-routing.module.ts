00fc73f9ff0288ef72079a7843ca3c1ce20367c5
Páez Ferreira Carlos David <cpaezfer@banrep.gov.co>
2024-01-24 13:30:36 -0500

    Add User Entitlements Routing

--- src/app/protected/modules/user-entitlements/user-entitlements-routing.module.ts
+++ src/app/protected/modules/user-entitlements/user-entitlements-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListUserEntitlementsComponent } from './pages/list-user-entitlements/list-user-entitlements.component';
+import { AdminUserEntitlementsComponent } from './pages/admin-user-entitlements/admin-user-entitlements.component';
+import { NotFoundComponent } from '../../../shared/components/not-found/not-found.component';
 
-const routes: Routes = [];
 
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
+const routes: Routes = [
+    {
+        path: 'list-user-entitlements',
+        component: ListUserEntitlementsComponent,
+        data: { title: 'JPM Lista de Títulos' },
+        pathMatch: 'full'
+    },
+    {
+        path: 'admin/:id',
+        component: AdminUserEntitlementsComponent,
+        pathMatch: 'full',
+        data: { title: 'JPM Detalle de Título' }
+    },
+    {
+        path: '', redirectTo: 'list-user-entitlements', pathMatch: 'full'
+    },
+    {
+        path: '**', component: NotFoundComponent,
+        data: { title: '404 - No encontrado' }
+    }
+];
+
+
+@NgModule( {
+    imports: [ RouterModule.forChild( routes ) ],
+    exports: [ RouterModule ]
+} )
 export class UserEntitlementsRoutingModule { }
