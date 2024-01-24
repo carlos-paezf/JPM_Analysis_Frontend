c1d511ac1d283a18b6b7ea1fd4bf4f0c56e91a31
Páez Ferreira Carlos David <cpaezfer@banrep.gov.co>
2024-01-24 13:31:36 -0500

    Update Products Module

--- src/app/protected/modules/products/products.module.ts
+++ src/app/protected/modules/products/products.module.ts
@@ -1,14 +1,25 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { MaterialDesignModule } from '../../../material-design/material-design.module';
+import { SharedModule } from '../../../shared/shared.module';
+import { AdminProductsComponent } from './pages/admin-products/admin-products.component';
+import { ListProductsComponent } from './pages/list-products/list-products.component';
 import { ProductsRoutingModule } from './products-routing.module';
 
 
-@NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    ProductsRoutingModule
-  ]
-})
+@NgModule( {
+    declarations: [
+        ListProductsComponent,
+        AdminProductsComponent
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        SharedModule,
+        MaterialDesignModule,
+        ProductsRoutingModule
+    ]
+} )
 export class ProductsModule { }
