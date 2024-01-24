05c3cda6b4fff41b3e7da1e9c93218277cf6a6d6
Páez Ferreira Carlos David <cpaezfer@banrep.gov.co>
2024-01-24 13:31:15 -0500

    Update Accounts Module

--- src/app/protected/modules/accounts/accounts.module.ts
+++ src/app/protected/modules/accounts/accounts.module.ts
@@ -1,14 +1,25 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { MaterialDesignModule } from '../../../material-design/material-design.module';
+import { SharedModule } from '../../../shared/shared.module';
 import { AccountsRoutingModule } from './accounts-routing.module';
+import { AdminAccountsComponent } from './pages/admin-accounts/admin-accounts.component';
+import { ListAccountsComponent } from './pages/list-accounts/list-accounts.component';
 
 
-@NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    AccountsRoutingModule
-  ]
-})
+@NgModule( {
+    declarations: [
+        AdminAccountsComponent,
+        ListAccountsComponent
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        SharedModule,
+        MaterialDesignModule,
+        AccountsRoutingModule
+    ]
+} )
 export class AccountsModule { }
