89f9c1cbda1bb97440d77ad8117d702fc71a1b41
Páez Ferreira Carlos David <cpaezfer@banrep.gov.co>
2024-01-24 13:30:10 -0500

    Add Clients Routing

--- src/app/protected/modules/clients/clients-routing.module.ts
+++ src/app/protected/modules/clients/clients-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from '../../../shared/components/not-found/not-found.component';
+import { ListClientsComponent } from './pages/list-clients/list-clients.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'list-clients',
+        component: ListClientsComponent,
+        data: { title: 'JPM Lista de Cuentas' },
+        pathMatch: 'full'
+    },
+    // {
+    //     path: 'admin/:number/:name',
+    //     component: AdminAccountsComponent,
+    //     pathMatch: 'full',
+    //     data: { title: 'JPM Detalle de Cuenta' }
+    // },
+    {
+        path: '', redirectTo: 'list-clients', pathMatch: 'full'
+    },
+    {
+        path: '**', component: NotFoundComponent,
+        data: { title: '404 - No encontrado' }
+    }
+];
 
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
+@NgModule( {
+    imports: [ RouterModule.forChild( routes ) ],
+    exports: [ RouterModule ]
+} )
 export class ClientsRoutingModule { }
