bd58f682a7a9e9536c235af974cb5c88f96769be
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-04-02 16:14:00 -0500

    Fix Functions Definitions

--- src/app/protected/modules/accounts/pages/list-accounts/list-accounts.component.ts
+++ src/app/protected/modules/accounts/pages/list-accounts/list-accounts.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { InfoTableBaseComponent } from '../../../../../shared/classes/sheet-base-component.class';
 import { AccountType, ColumnTableType } from '../../../../../shared/types';
 import { AccountsService } from '../../services/accounts.service';
+import { AppUtilsMessagesService } from 'src/app/shared/services/app-utils-messages.service';
 
 
 @Component( {
@@ -12,27 +13,30 @@ import { AccountsService } from '../../services/accounts.service';
 } )
 export class ListAccountsComponent extends InfoTableBaseComponent<AccountType> implements OnInit {
     public override displayedColumns: string[] = [
-        'account_number',
-        'account_name',
-        'account_type',
-        'bank_currency',
-        'updated_at',
-        'deleted_at',
+        'accountNumber',
+        'accountName',
+        'accountType',
+        'bankCurrency',
+        'updatedAt',
+        'deletedAt',
     ];
 
     public override columns: ColumnTableType<AccountType>[] = [
-        { columnDef: 'account_number', header: 'Número de cuenta', cell: ( row ) => row.account_number },
-        { columnDef: 'account_name', header: 'Nombre de cuenta', cell: ( row ) => row.account_name },
-        { columnDef: 'account_type', header: 'Tipo de cuenta', cell: ( row ) => row.account_type },
-        { columnDef: 'bank_currency', header: 'Bank Currency', cell: ( row ) => row.bank_currency },
-        { columnDef: 'updated_at', header: 'Última Actualización', cell: ( row ) => row.updated_at },
-        { columnDef: 'deleted_at', header: 'Fecha de Desactivación', cell: ( row ) => row.deleted_at },
+        { columnDef: 'accountNumber', header: 'Número de cuenta', cell: ( row ) => row.accountNumber },
+        { columnDef: 'accountName', header: 'Nombre de cuenta', cell: ( row ) => row.accountName },
+        { columnDef: 'accountType', header: 'Tipo de cuenta', cell: ( row ) => row.accountType },
+        { columnDef: 'bankCurrency', header: 'Bank Currency', cell: ( row ) => row.bankCurrency },
+        { columnDef: 'updatedAt', header: 'Última Actualización', cell: ( row ) => row.updatedAt },
+        { columnDef: 'deletedAt', header: 'Fecha de Desactivación', cell: ( row ) => row.deletedAt },
     ];
 
     public deleteAccount!: Function;
     public reactivateAccount!: Function;
 
-    constructor ( private _accountsService: AccountsService ) {
+    constructor (
+        private readonly _appUtilsMessagesService: AppUtilsMessagesService,
+        private _accountsService: AccountsService
+    ) {
         super();
     }
 
@@ -42,13 +46,60 @@ export class ListAccountsComponent extends InfoTableBaseComponent<AccountType> i
      * accordingly.
      */
     ngOnInit (): void {
-        this._accountsService.getAccounts()
-            .subscribe( ( response ) => {
-                this.data = response.data;
-                this.isEmptyTable = ( response.data.length <= 0 );
-                this.isLoadingResults = false;
-            } );
-        this.deleteAccount = this._accountsService.deleteAccount;
-        this.reactivateAccount = this._accountsService.reactivateAccount;
+        this.loadData();
+        this.defineSoftDeleteFunctions();
     }
+
+
+    /**
+     * The function `loadData` retrieves data from an API using an accounts service in TypeScript and
+     * updates the component's data and loading state accordingly.
+     */
+    loadData (): void {
+        this._accountsService.getAll()
+            .subscribe(
+                {
+                    next: ( response ) => {
+                        this.data = response.data;
+                        this.isEmptyTable = response.totalResults <= 0;
+                        this.isLoadingResults = false;
+                    },
+                    error: ( error ) => {
+                        this.isLoadingResults = false;
+                        this._appUtilsMessagesService.showUpdateErrorMessage( error );
+                    },
+                }
+            );
+    }
+
+
+    /**
+     * The `defineSoftDeleteFunctions` function defines methods to delete and reactivate accounts with
+     * error handling and data reloading.
+     */
+    defineSoftDeleteFunctions (): void {
+        this.deleteAccount = ( id: string ) => this._accountsService.deactivate( id ).subscribe(
+            {
+                next: ( e ) => {
+                    this.loadData();
+                    this._accountsService.notifyUpdate();
+                },
+                error: ( error ) => {
+                    console.error( 'Error deleting account:', error );
+                }
+            }
+        );
+        this.reactivateAccount = ( id: string ) => this._accountsService.reactivate( id ).subscribe(
+            {
+                next: ( e ) => {
+                    this.loadData();
+                    this._accountsService.notifyUpdate();
+                },
+                error: ( error ) => {
+                    console.error( 'Error deleting account:', error );
+                }
+            }
+        );
+    }
+
 }
