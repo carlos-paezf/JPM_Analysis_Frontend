7843e7c394d9e0d51f309d68ec1b56ed349ed3f5
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-04-02 15:56:36 -0500

    Update Suport Image Component

--- src/app/shared/components/support-image/support-image.component.ts
+++ src/app/shared/components/support-image/support-image.component.ts
@@ -1,4 +1,7 @@
+import { Location } from '@angular/common';
 import { Component, Input } from '@angular/core';
+
+import { HttpStatusService } from 'src/app/shared/services/http-status.service';
 
 /**
  * The SupportImageComponent class is a TypeScript component that represents 
@@ -17,4 +20,21 @@ export class SupportImageComponent {
     @Input() public imgSrc!: string;
     @Input() public alt!: string;
     @Input() public message?: string = "No se ha encontrado la información solicitada. Por favor, valida que la información se encuentra en la lista correspondiente";
+
+
+    constructor (
+        private readonly _location: Location,
+        private readonly _httpStatusService: HttpStatusService
+    ) { }
+
+
+    goBack () {
+        this._httpStatusService.removeNotFoundError();
+        return this._location.back();
+    }
+
+    goHome () {
+        this._httpStatusService.removeNotFoundError();
+        window.location.href = "/";
+    }
 }
