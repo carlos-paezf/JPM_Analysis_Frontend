1992653e55a41e8f89aa8a32d251077f2b389d86
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-04-03 15:08:54 -0500

    Update Products Service

--- src/app/protected/modules/products/services/products.service.ts
+++ src/app/protected/modules/products/services/products.service.ts
@@ -1,65 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ProductType, ResponseSheetsType } from '../../../../shared/types';
-import { Observable, delay, of } from 'rxjs';
-import { PRODUCTS } from '../../../../shared/mocks';
 
+import { BaseServiceWithSoftDelete } from '../../../../shared/classes/base-service.class';
+import { ProductEagerType, ProductType } from '../../../../shared/types';
+
+
+/**
+ * @author cpaezfer
+ */
 @Injectable( {
     providedIn: 'root'
 } )
-export class ProductsService {
-
-    public getProducts (): Observable<ResponseSheetsType<ProductType>> {
-        const response: ResponseSheetsType<ProductType> = {
-            data: PRODUCTS,
-            totalResults: PRODUCTS.length
-        };
-
-        return of( response ).pipe( delay( Math.random() * 1000 ) );
-    }
-
-    public getProductById ( id: string ): Observable<ProductType | null> {
-        const response = PRODUCTS.find( user => user.id === id ) || null;
-
-        return of( response ).pipe( delay( Math.random() * 1000 ) );
-    }
-
-    public updateProduct ( id: string, updatedData: ProductType ): Observable<boolean> {
-        const index = PRODUCTS.findIndex( product => product.id === id );
-
-        if ( index !== -1 ) {
-            PRODUCTS[ index ] = { ...updatedData, id: PRODUCTS[ index ].id, updatedAt: new Date() };
-        }
-
-        return of( true );
-    }
-
-    public deleteProduct ( id: string ): Observable<boolean> {
-        const index = PRODUCTS.findIndex( product => product.id === id );
-
-        if ( index !== -1 ) {
-            PRODUCTS[ index ] = { ...PRODUCTS[ index ], deletedAt: new Date() };
-        }
-
-        return of( true );
-    }
-
-    public reactivateProduct ( id: string ): Observable<boolean> {
-        const index = PRODUCTS.findIndex( product => product.id === id );
-
-        if ( index !== -1 ) {
-            PRODUCTS[ index ] = { ...PRODUCTS[ index ], deletedAt: null };
-        }
-
-        return of( true );
-    }
-
-    public getProductsByProfileId ( profileId: string ): Observable<ResponseSheetsType<ProductType>> {
-        const response: ResponseSheetsType<ProductType> = {
-            data: PRODUCTS.filter( ( element ) => element.id === profileId ),
-            totalResults: PRODUCTS.length
-        };
-
-        return of( response ).pipe( delay( Math.random() * 1000 ) );
-
+export class ProductsService extends BaseServiceWithSoftDelete<ProductType, ProductEagerType> {
+    constructor ( _http: HttpClient ) {
+        super( _http, "products" );
     }
 }
