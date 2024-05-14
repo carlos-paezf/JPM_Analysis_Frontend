3368e424c00f3d4d74410dfeae785b3d371aff9f
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-05-14 10:31:18 -0500

    Refactor Code

--- src/app/protected/modules/company-users/services/company-users.service.ts
+++ src/app/protected/modules/company-users/services/company-users.service.ts
@@ -1,68 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, delay, of } from 'rxjs';
 
-import { COMPANY_USERS } from '../../../../shared/mocks';
-import { CompanyUserType, ResponseSheetsType } from '../../../../shared/types';
+import { BaseServiceWithSoftDelete } from '../../../../shared/classes/base-service.class';
+import { CompanyUserEagerType, CompanyUserType } from '../../../../shared/types';
 
 
+/**
+ * @author cpaezfer
+ */
 @Injectable( {
     providedIn: 'root'
 } )
-export class CompanyUsersService {
-    constructor () { }
-
-    public getCompanyUsers (): Observable<ResponseSheetsType<CompanyUserType>> {
-        const response: ResponseSheetsType<CompanyUserType> = {
-            data: COMPANY_USERS,
-            totalResults: COMPANY_USERS.length
-        };
-
-        return of( response ).pipe( delay( Math.random() * 1000 ) );
-    }
-
-    public getCompanyUserByAccessId ( accessId: string ): Observable<CompanyUserType | null> {
-        const response = COMPANY_USERS.find( user => user.accessId === accessId ) || null;
-
-        return of( response ).pipe( delay( Math.random() * 1000 ) );
-    }
-
-    public updateCompanyUser ( accessId: string, updatedData: CompanyUserType ): Observable<boolean> {
-        const index = COMPANY_USERS.findIndex( companyUser => companyUser.accessId === accessId );
-
-        if ( index !== -1 ) {
-            COMPANY_USERS[ index ] = { ...updatedData, accessId: COMPANY_USERS[ index ].accessId, updatedAt: new Date() };
-        }
-
-        return of( true );
-    }
-
-    public deleteCompanyUser ( accessId: string ): Observable<boolean> {
-        const index = COMPANY_USERS.findIndex( companyUser => companyUser.accessId === accessId );
-
-        if ( index !== -1 ) {
-            COMPANY_USERS[ index ] = { ...COMPANY_USERS[ index ], deletedAt: new Date() };
-        }
-
-        return of( true );
-    }
-
-    public reactivateCompanyUser ( accessId: string ): Observable<boolean> {
-        const index = COMPANY_USERS.findIndex( companyUser => companyUser.accessId === accessId );
-
-        if ( index !== -1 ) {
-            COMPANY_USERS[ index ] = { ...COMPANY_USERS[ index ], deletedAt: null };
-        }
-
-        return of( true );
-    }
-
-    public getCompanyUsersByProfileId ( profileId: string ): Observable<ResponseSheetsType<CompanyUserType>> {
-        const response: ResponseSheetsType<CompanyUserType> = {
-            data: COMPANY_USERS.filter( ( element ) => element.profileId === profileId ),
-            totalResults: COMPANY_USERS.length
-        };
-
-        return of( response ).pipe( delay( Math.random() * 1000 ) );
-
+export class CompanyUsersService extends BaseServiceWithSoftDelete<CompanyUserType, CompanyUserEagerType> {
+    constructor ( _http: HttpClient ) {
+        super( _http, "companyUsers" );
     }
 }
