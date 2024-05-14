0e0ca11bc61bdd24a9cd1cb06068d06c82eaaeba
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-05-14 10:50:20 -0500

    Add Get All Method

--- src/app/protected/modules/products-accounts/services/products-accounts.service.ts
+++ src/app/protected/modules/products-accounts/services/products-accounts.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 
 import { BaseServiceWithSoftDelete } from '../../../../shared/classes/base-service.class';
 import { ProductAccountEagerType, ProductAccountType } from '../../../../shared/types';
+import { Observable } from 'rxjs';
+import { ResponseSheetsType } from '../../../../shared/types/index';
 
 
 /**
@@ -14,5 +16,13 @@ import { ProductAccountEagerType, ProductAccountType } from '../../../../shared/
 export class ProductsAccountsService extends BaseServiceWithSoftDelete<ProductAccountType, ProductAccountEagerType> {
     constructor ( _http: HttpClient ) {
         super( _http, "productsAccounts" );
+    }
+
+    /**
+     * The function `getAll` makes an HTTP GET request to retrieve eager-loaded product account data.
+     * @returns An Observable of type ResponseSheetsType<ProductAccountEagerType> is being returned.
+     */
+    public override getAll (): Observable<ResponseSheetsType<ProductAccountEagerType>> {
+        return this._http.get<ResponseSheetsType<ProductAccountEagerType>>( `${ this._baseUrl }/eager` );
     }
 }
