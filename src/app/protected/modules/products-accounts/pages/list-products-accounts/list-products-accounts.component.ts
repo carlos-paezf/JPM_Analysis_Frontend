6e52b9b2d12fb08ab4515df0e88b203fb66770d6
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-05-14 10:49:56 -0500

    Refactor Code

--- src/app/protected/modules/products-accounts/pages/list-products-accounts/list-products-accounts.component.ts
+++ src/app/protected/modules/products-accounts/pages/list-products-accounts/list-products-accounts.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductAccountEagerType, ProductAccountType, ColumnTableType } from '../../../../../shared/types';
-import { ProductsAccountsService } from '../../services/products-accounts.service';
+
 import { InfoTableBaseComponent } from '../../../../../shared/classes/sheet-base-component.class';
+import { AppUtilsMessagesService } from '../../../../../shared/services/app-utils-messages.service';
+import { ColumnTableType, ProductAccountEagerType } from '../../../../../shared/types';
+import { ProductsAccountsService } from '../../services/products-accounts.service';
+
 
 @Component( {
     selector: 'app-list-products-accounts',
@@ -10,23 +13,30 @@ import { InfoTableBaseComponent } from '../../../../../shared/classes/sheet-base
 } )
 export class ListProductsAccountsComponent extends InfoTableBaseComponent<ProductAccountEagerType> implements OnInit {
     public override displayedColumns: string[] = [
-        'productId',
         'accountNumber',
+        'accountName',
+        'productName',
+        'subProduct',
         'updatedAt',
         'deletedAt',
     ];
 
     public override columns: ColumnTableType<ProductAccountEagerType>[] = [
-        { columnDef: 'productId', header: 'Product Id', cell: ( row ) => row.product.subProduct ?? row.product.productName },
-        { columnDef: 'accountNumber', header: 'Account Number', cell: ( row ) => row.accountNumber ? `${ row.account.accountNumber } - ${ row.account.accountName }` : '' },
+        { columnDef: 'accountNumber', header: 'Account Number', cell: ( row ) => row.accountNumber ?? '' },
+        { columnDef: 'accountName', header: 'Account Name', cell: ( row ) => row.account?.accountName ?? '' },
+        { columnDef: 'productName', header: 'Product Name', cell: ( row ) => row.product?.productName ?? '' },
+        { columnDef: 'subProduct', header: 'Sub Product', cell: ( row ) => row.product?.subProduct ?? '' },
         { columnDef: 'updatedAt', header: 'Última Actualización', cell: ( row ) => row.updatedAt },
         { columnDef: 'deletedAt', header: 'Fecha de Desactivación', cell: ( row ) => row.deletedAt },
     ];
 
-    public deleteClient!: Function;
-    public reactivateClient!: Function;
+    public deactivateItem!: Function;
+    public reactivateItem!: Function;
 
-    constructor ( private _clientsService: ProductsAccountsService ) {
+    constructor (
+        private _appUtilsMessagesService: AppUtilsMessagesService,
+        private _productsAccountsService: ProductsAccountsService
+    ) {
         super();
     }
 
@@ -36,13 +46,63 @@ export class ListProductsAccountsComponent extends InfoTableBaseComponent<Produc
      * accordingly.
      */
     ngOnInit (): void {
-        this._clientsService.getProductsAccountsStrategy()
-            .subscribe( ( response ) => {
-                this.data = response.data;
-                this.isEmptyTable = ( response.data.length <= 0 );
-                this.isLoadingResults = false;
-            } );
-        this.deleteClient = this._clientsService.deactivateProductAccount;
-        this.reactivateClient = this._clientsService.reactivateProductAccount;
+        this._loadData();
+
+        this._defineSoftDeleteFunctions();
     };
+
+
+    /**
+     * The _loadData function fetches data from a service and updates the component's properties
+     * accordingly.
+     */
+    private _loadData (): void {
+        this._productsAccountsService.getAll().subscribe(
+            {
+                next: ( response ) => {
+                    this.data = response.data;
+                    this.isEmptyTable = response.totalResults <= 0;
+                    this.isLoadingResults = false;
+                },
+                error: ( error ) => {
+                    this.isLoadingResults = false;
+                    this._appUtilsMessagesService.showQueryErrorMessage( error );
+                }
+            }
+        );
+    }
+
+
+    /**
+     * The `defineSoftDeleteFunctions` function defines methods to deactivate and reactivate items
+     * using a service.
+     */
+    private _defineSoftDeleteFunctions (): void {
+        this.deactivateItem = ( id: string ) => this._productsAccountsService.deactivate( id )
+            .subscribe( this._subscriptionObject() );
+
+        this.reactivateItem = ( id: string ) => this._productsAccountsService.reactivate( id )
+            .subscribe( this._subscriptionObject() );
+    }
+
+
+    /**
+     * The `_subscriptionObject` function returns an object with `next` and `error` methods that
+     * perform certain actions when called.
+     * @returns An object with two properties: `next` and `error`. The `next` property is a function
+     * that calls the `_loadData()` method and then notifies the service of an update. The `error`
+     * property is a function that calls the `_appUtilsMessagesService.showUpdateErrorMessage()` method
+     * with an error parameter.
+     */
+    private _subscriptionObject () {
+        return {
+            next: () => {
+                this._loadData();
+                this._productsAccountsService.notifyUpdate();
+            },
+            error: ( error: any ) => {
+                this._appUtilsMessagesService.showQueryErrorMessage( error );
+            }
+        };
+    }
 }
