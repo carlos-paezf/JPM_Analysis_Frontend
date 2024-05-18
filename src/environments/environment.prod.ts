1dc46b1841b54d30e8cdc606395fb85ac6d7d28a
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-05-17 20:36:57 -0500

    Update Environments

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    baseURL: 'https://localhost:7239'
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    baseURL: 'https://localhost:7239'
-};
