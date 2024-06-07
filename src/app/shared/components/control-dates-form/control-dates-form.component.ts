9e2df2e6c7b588f3f649b4c1792c96f751a9566b
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-06-06 20:02:50 -0500

    Fix Control Date

--- src/app/shared/components/control-dates-form/control-dates-form.component.ts
+++ src/app/shared/components/control-dates-form/control-dates-form.component.ts
@@ -10,4 +10,16 @@ export class ControlDatesFormComponent {
     @Input() createdAt!: Date;
     @Input() updatedAt!: Date;
     @Input() deletedAt: Date | undefined | null;
+
+    /**
+     * The function `formatDate` takes a Date object or null as input and returns a string
+     * representation of the date if it is not null.
+     * @param {Date | null} date - The `date` parameter in the `formatDate` function is of type `Date`
+     * or `null`, meaning it can either be a valid date object or `null`.
+     * @returns The function `formatDate` is returning the string representation of the input date if
+     * it is not null. If the input date is null, the function will return null.
+     */
+    public formatDate ( date: Date | null ) {
+        return date && new Date( date ).toString();
+    }
 }
