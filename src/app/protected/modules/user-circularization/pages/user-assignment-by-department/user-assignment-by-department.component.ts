f84fd66ee559ed99f0d22dc3b858b193a43035ac
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-07-25 20:48:18 -0500

    "Updated user-assignment-by-department component with HTML, SCSS, and TS changes"

--- src/app/protected/modules/user-circularization/pages/user-assignment-by-department/user-assignment-by-department.component.ts
+++ src/app/protected/modules/user-circularization/pages/user-assignment-by-department/user-assignment-by-department.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { DepartmentsService } from '../../../departments/services/departments.service';
+import { CompanyUsersService } from '../../../company-users/services/company-users.service';
+import { CompanyUserType, DepartmentType } from '../../../../../shared/types';
+import { forkJoin } from 'rxjs';
+import { AppUtilsMessagesService } from '../../../../../shared/services/app-utils-messages.service';
+
 
 @Component( {
-  selector: 'app-user-assignment-by-department',
-  templateUrl: './user-assignment-by-department.component.html',
-  styleUrls: [ './user-assignment-by-department.component.scss' ]
+    selector: 'app-user-assignment-by-department',
+    templateUrl: './user-assignment-by-department.component.html',
+    styleUrls: [ './user-assignment-by-department.component.scss' ]
 } )
-export class UserAssignmentByDepartmentComponent {
+export class UserAssignmentByDepartmentComponent implements OnInit {
+    public departments: DepartmentType[] = [];
+    public companyUsers: CompanyUserType[] = [];
+    public isLoading: boolean = true;
 
+
+    constructor (
+        private readonly _departmentsService: DepartmentsService,
+        private readonly _companyUsersService: CompanyUsersService,
+        private readonly _appUtilsMessagesService: AppUtilsMessagesService
+    ) { }
+
+
+    ngOnInit (): void {
+        this.isLoading = true;
+
+        forkJoin(
+            [
+                this._departmentsService.getAll(),
+                this._companyUsersService.getAll()
+            ]
+        ).subscribe( {
+            next: ( [ departmentResponse, companyUserResponse ] ) => {
+                this.departments = departmentResponse.data;
+                this.companyUsers = companyUserResponse.data;
+                this.isLoading = false;
+            },
+            error: ( error ) => {
+                this.isLoading = false;
+                this._appUtilsMessagesService.showQueryErrorMessage( error );
+            }
+        } );
+
+    }
 }
