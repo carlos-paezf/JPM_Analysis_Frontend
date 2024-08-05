a2a7d699d1c92e5964c75a914705252699483676
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-08-05 13:30:50 -0500

    Update User Circularization Module

--- src/app/protected/modules/user-circularization/user-circularization.module.ts
+++ src/app/protected/modules/user-circularization/user-circularization.module.ts
@@ -4,17 +4,19 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { MaterialDesignModule } from '../../../material-design/material-design.module';
 import { SharedModule } from '../../../shared/shared.module';
+import { AdminDepartmentComponent } from './pages/admin-department/admin-department.component';
 import { GenerateUserCircularizationComponent } from './pages/generate-user-circularization/generate-user-circularization.component';
-import { ListCompanyUsersDepartmentsComponent } from './pages/list-company-users-departments/list-company-users-departments.component';
+import { ListDepartmentsComponent } from './pages/list-departments/list-departments.component';
 import { UserAssignmentByDepartmentComponent } from './pages/user-assignment-by-department/user-assignment-by-department.component';
 import { UserCircularizationRoutingModule } from './user-circularization-routing.module';
 
 
 @NgModule( {
     declarations: [
-        ListCompanyUsersDepartmentsComponent,
+        ListDepartmentsComponent,
         GenerateUserCircularizationComponent,
-        UserAssignmentByDepartmentComponent
+        UserAssignmentByDepartmentComponent,
+        AdminDepartmentComponent
     ],
     imports: [
         CommonModule,
