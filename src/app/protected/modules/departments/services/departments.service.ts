332dd41732711fd41f43b5d0fafb8b7e9e4e9827
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-08-05 11:27:36 -0500

    Update Inheritance

--- src/app/protected/modules/departments/services/departments.service.ts
+++ src/app/protected/modules/departments/services/departments.service.ts
@@ -1,12 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BaseService } from '../../../../shared/classes/base-service.class';
+import { BaseServiceWithSoftDelete } from '../../../../shared/classes/base-service.class';
 import { DepartmentEagerType, DepartmentType } from '../../../../shared/types';
 
 @Injectable( {
     providedIn: 'root'
 } )
-export class DepartmentsService extends BaseService<DepartmentType, DepartmentEagerType> {
+export class DepartmentsService extends BaseServiceWithSoftDelete<DepartmentType, DepartmentEagerType> {
     constructor ( _http: HttpClient, ) {
         super( _http, "departments" );
     }
