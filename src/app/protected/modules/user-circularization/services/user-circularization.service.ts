e50be6d9f19f7917efa4eab8a2612b3c8ce47bc0
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-08-08 17:09:28 -0500

    Add Documentation

--- src/app/protected/modules/user-circularization/services/user-circularization.service.ts
+++ src/app/protected/modules/user-circularization/services/user-circularization.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { environment } from '../../../../../environments/environment';
-import { DepartmentType, UserDepartmentAssignmentType } from '../../../../shared/types/index';
+import { CircularizationData, DepartmentType, UserDepartmentAssignmentType } from '../../../../shared/types/index';
 
 
 @Injectable( {
@@ -17,12 +18,43 @@ export class UserCircularizationService {
     }
 
 
+    /**
+     * The function `assignDepartmentToCompanyUsers` sends a PATCH request to assign departments to
+     * company users.
+     * @param {UserDepartmentAssignmentType[]} body - The `body` parameter in the
+     * `assignDepartmentToCompanyUsers` function is an array of objects of type
+     * `UserDepartmentAssignmentType`. Each object in the array represents an assignment of a
+     * department to a company user.
+     * @returns The `assignDepartmentToCompanyUsers` function is returning the result of a PATCH
+     * request made to the specified URL `${ this._baseURL
+     * }/userCircularization/assign-department-to-company-users` with the provided `body` data.
+     */
     public assignDepartmentToCompanyUsers ( body: UserDepartmentAssignmentType[] ) {
         return this._http.patch( `${ this._baseURL }/userCircularization/assign-department-to-company-users`, body );
     }
 
 
+    /**
+     * The function `addDepartment` sends a POST request to add a new department using the provided
+     * department data.
+     * @param {DepartmentType} department - The `addDepartment` function takes a parameter `department`
+     * of type `DepartmentType`. This parameter represents the department object that will be added to
+     * the system. The function sends a POST request to the specified URL endpoint to add the
+     * department using the provided department object.
+     * @returns The `addDepartment` function is returning the result of an HTTP POST request to the
+     * specified URL `${ this._baseURL }/departments` with the `department` object as the payload.
+     */
     public addDepartment ( department: DepartmentType ) {
         return this._http.post( `${ this._baseURL }/departments`, department );
+    }
+
+
+    /**
+     * This function generates user circularization data by making an HTTP GET request to a specific
+     * endpoint.
+     * @returns An Observable of CircularizationData array is being returned.
+     */
+    public generateUserCircularization (): Observable<CircularizationData[]> {
+        return this._http.get<CircularizationData[]>( `${ this._baseURL }/userCircularization/generate-user-circularization` );
     }
 }
